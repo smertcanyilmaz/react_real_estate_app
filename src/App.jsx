35ce96d388f0d1073315f7ad59f89c09f0f60387
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-09-13 15:27:44 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="">test</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: "font-family: 'Poppins', sans-serif",
+    },
+  },
+  plugins: [],
+};

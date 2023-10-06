19318b8a724cd2131960d688975887a236bb9298
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-10-06 09:55:54 +0300

    add firebase login and navigate

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,11 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AuthEntranceSide from "../../../components/AuthEntranceSide/AuthEntranceSide";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = ({ setUnAuthNavbar }) => {
+  const navigate = useNavigate();
   useEffect(() => {
     setUnAuthNavbar(true);
   }, []);
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const auth = getAuth();
+
+  const { email, password } = user;
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((user) => {
+        console.log(user);
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <div className="flex max-w-full ">
       <AuthEntranceSide />
@@ -20,6 +40,9 @@ const Login = ({ setUnAuthNavbar }) => {
               id="email"
               className="bg-transparent border border-gray-400/60 w-full h-12 pl-3 rounded-md"
               placeholder="E-mail"
+              onChange={(e) => {
+                setUser({ ...user, email: e.target.value });
+              }}
             />
             <input
               type="password"
@@ -27,8 +50,14 @@ const Login = ({ setUnAuthNavbar }) => {
               id="password"
               className="bg-transparent border border-gray-400/60 w-full h-12 pl-3 rounded-md"
               placeholder="Password"
+              onChange={(e) => {
+                setUser({ ...user, password: e.target.value });
+              }}
             />
-            <button className="w-24 h-12 text-white bg-[#36cf94] rounded-md mb-5">
+            <button
+              onClick={handleSignIn}
+              className="w-24 h-12 text-white bg-[#36cf94] rounded-md mb-5"
+            >
               SIGN IN
             </button>
           </form>

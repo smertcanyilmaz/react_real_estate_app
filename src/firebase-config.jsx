80504d6bfbd46c72f39e6af2b0bbf6bc0108c5fc
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-12-10 14:54:41 +0300

    add .env

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -3,23 +3,13 @@ import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDskyVktJyirfaJAuXaEeBkyZQ9nOXqrpE",
-  authDomain: "real-estate-app-b1e5c.firebaseapp.com",
-  projectId: "real-estate-app-b1e5c",
-  storageBucket: "real-estate-app-b1e5c.appspot.com",
-  messagingSenderId: "591740044627",
-  appId: "1:591740044627:web:9cbde1af3f282e32ba8da6",
+  apiKey: import.meta.env.VITE_REACT_APP_apiKey,
+  authDomain: import.meta.env.VITE_REACT_APP_authDomain,
+  projectId: import.meta.env.VITE_REACT_APP_projectId,
+  storageBucket: import.meta.env.VITE_REACT_APP_storageBucket,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_messagingSenderId,
+  appId: import.meta.env.VITE_REACT_APP_appId,
 };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCvBC6dse5PuOUsTCj0H88hePlWPqoOB8M",
-//   authDomain: "real-estate-2-bd95b.firebaseapp.com",
-//   projectId: "real-estate-2-bd95b",
-//   storageBucket: "real-estate-2-bd95b.appspot.com",
-//   messagingSenderId: "508363569510",
-//   appId: "1:508363569510:web:573a797c0232e3bf65b0a7",
-//   measurementId: "G-5YJKGZEGD2",
-// };
 
 const app = initializeApp(firebaseConfig);
 

e27c110ab1eaa481f7d27605230f040f30ab8152
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-12-12 14:15:43 +0300

    Last update before responsive section

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 
 const Layout = ({ unAuthNavbar }) => {
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, []);
-
   return (
-    <div className="min-h-screen max-w-full flex flex-col gap-20 ">
+    <div className="min-h-screen max-w-full flex flex-col gap-20">
       <div
         className={`flex-1 max-w-6xl mx-auto flex flex-col 
         ${unAuthNavbar && "max-w-full"}

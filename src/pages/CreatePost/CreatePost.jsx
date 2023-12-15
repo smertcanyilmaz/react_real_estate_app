debf7b48a6fcbc12a6630a484de4f50cc0bdef4d
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-12-16 02:55:33 +0300

    update

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -6,7 +6,7 @@ const CreatePost = () => {
   return (
     <div className="max-w-6xl mt-10 mb-10 flex flex-col gap-10">
       <PostLine custom="adInfo" />
-      <CreatePostInfo />
+      {/* <CreatePostInfo /> */}
     </div>
   );
 };

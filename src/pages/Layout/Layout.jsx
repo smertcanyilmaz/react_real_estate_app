096991ed3d26c28e0b8f7cb3809f3538b8f0f400
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-12-16 16:07:13 +0300

    update

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -2,56 +2,27 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
-import EngineeringIcon from "@mui/icons-material/Engineering";
 
 const Layout = ({ unAuthNavbar }) => {
-  const [toggle, setToggle] = useState(false);
+  // const [toggle, setToggle] = useState(false);
 
-  useEffect(() => {
-    document.body.style.overflow = !toggle ? "hidden" : "auto";
-  }, [toggle]);
+  // useEffect(() => {
+  //   document.body.style.overflow = !toggle ? "hidden" : "auto";
+  // }, [toggle]);
 
   return (
-    <>
+    <div className="min-h-screen max-w-full flex flex-col gap-20 ">
       <div
-        className={`md:hidden fixed top-0 left-0 right-0 bottom-0" w-screen h-screen bg-black opacity-80 ${
-          toggle ? "hidden" : ""
-        }`}
-      ></div>
-      <div
-        className={`absolute md:hidden  top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-2/3 h-1/2 text-center  bg-gray-200 p-3 rounded-md ${
-          toggle ? "hidden" : ""
-        } `}
-      >
-        <div className="w-full h-full flex flex-col items-center justify-center gap-10">
-          <div className="w-full flex flex-col items-center gap-5 ">
-            <EngineeringIcon fontSize="large" />
-            <h3 className="capitalize font-semibold text-gray-800">
-              mobile design is being developed
-            </h3>
-            <p className="text-sm">Please continue on your computer</p>
-          </div>
-          <button
-            onClick={() => setToggle((prev) => !prev)}
-            className="capitalize w-2/3 h-1/5  bg-gray-800 text-gray-100 rounded-md text-sm "
-          >
-            go ahead anyway
-          </button>
-        </div>
-      </div>
-      <div className="min-h-screen max-w-full flex flex-col gap-20 ">
-        <div
-          className={`flex-1 max-w-6xl mx-auto flex flex-col
+        className={`flex-1 max-w-6xl mx-auto flex flex-col
         ${unAuthNavbar && "max-w-full"}
        `}
-        >
-          {!unAuthNavbar && <Navbar />}
+      >
+        {!unAuthNavbar && <Navbar />}
 
-          <Outlet />
-        </div>
-        {!unAuthNavbar && <Footer />}
-      </div>{" "}
-    </>
+        <Outlet />
+      </div>
+      {!unAuthNavbar && <Footer />}
+    </div>
   );
 };
 

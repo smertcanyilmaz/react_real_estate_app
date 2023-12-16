c1f10d41c2255ed8a5395e5645329b79ab13f4dd
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-12-16 07:19:13 +0300

    update info popup

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -16,21 +16,18 @@ const Layout = ({ unAuthNavbar }) => {
         }`}
       >
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[18rem] h-1/2 bg-gray-200/80 p-3 rounded-md">
-          <div className="w-full h-full flex flex-col items-center gap-8">
-            <div
-              onClick={() => setToggle((prev) => !prev)}
-              className="close w-full"
-            >
-              <CloseIcon />
-            </div>
-            <div className="w-full flex flex-col items-center justify-center gap-8">
+          <div className="w-full h-full flex flex-col items-center justify-center gap-10">
+            <div className="w-full flex flex-col items-center gap-5 ">
               <EngineeringIcon fontSize="large" />
               <h3 className="capitalize font-semibold text-gray-800">
                 mobile design is being developed
               </h3>
               <p className="text-sm">Please continue on your computer</p>
             </div>
-            <button className="capitalize w-2/3 h-16  bg-gray-800 text-gray-100 rounded-md text-sm ">
+            <button
+              onClick={() => setToggle((prev) => !prev)}
+              className="capitalize w-2/3 h-1/5  bg-gray-800 text-gray-100 rounded-md text-sm "
+            >
               go ahead anyway
             </button>
           </div>

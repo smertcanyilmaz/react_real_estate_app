304bb8a9748b813af6e419ff2eba24c0b14d74d3
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-01-03 21:34:11 +0300

    update mobile menu pathname

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -24,7 +24,7 @@ const MobileMenu = () => {
   useEffect(() => {
     let prevScrollPos = window.scrollY;
 
-    if (location.pathname === "/profilemenu") {
+    if (location.pathname !== "/profilemenu") {
       const handleScroll = () => {
         const currentScrollPos = window.scrollY;
         const shouldShow =

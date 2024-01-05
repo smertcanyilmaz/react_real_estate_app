0c5de36de47bba4562907a1afba9deb237bd5230
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-01-05 21:49:11 +0300

    update favorites

--- src/pages/Profile/Favorites/Favorites.jsx
+++ src/pages/Profile/Favorites/Favorites.jsx
@@ -1,18 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import ProfileTemplate from "../../../components/ProfileTemplate/ProfileTemplate";
 import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 import ProfileProductCard from "../../../components/ProfileProductCard/ProfileProductCard";
 import { ContextProfile } from "../../../Context/ProfileContext";
 import "./Favorites.css";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Favorites = () => {
   const { favoriteEstates, loadingFav } = useContext(ContextProfile);
+  const pathname = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/posts/actives" && window.innerWidth <= 640)
+      document.body.style.backgroundColor = "#ffffff";
+    return () => {
+      document.body.style.backgroundColor = "#e3e3e1";
+    };
+  }, [pathname]);
 
   return (
     <ProfileTemplate>
-      <div className="favorites w-full h-full bg-gray-50 flex flex-col items-center justify-center gap-3 rounded-sm border border-gray-400/50">
+      <div onClick={() => navigate("/profilemenu")} className="md:hidden p-3">
+        <WestOutlinedIcon />
+      </div>
+      <div className="active_posts w-[95%] md:w-full mx-auto md:mx-none mt-3 md:mt-0 h-full flex flex-col justify-between gap-3 bg-[--bg_color] rounded-lg md:rounded-none">
         <div
-          className={`note w-full h-full  bg-gray-50 flex flex-col gap-5 border border-gray-400/50 ${
+          className={`note w-full h-full  bg-gray-50 flex flex-col gap-5 border border-gray-400/50 rounded-lg md:rounded-none ${
             favoriteEstates.length === 0 && "not_post"
           }`}
         >

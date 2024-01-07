1f6f3d587d62f341c5d7422765bfaf945a24c93d
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-01-07 15:58:06 +0300

    update

--- src/components/MobileEstatesSlider/MobileEstatesSlider.jsx
+++ src/components/MobileEstatesSlider/MobileEstatesSlider.jsx
@@ -76,7 +76,7 @@ const MobileEstatesSlider = ({ selectedButtonHandler, selectedButton }) => {
     dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 2,
     arrows: false,
     responsive: [

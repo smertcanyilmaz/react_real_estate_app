df9c39b9f3985cf663e02355d3fd13095686741f
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-01-10 15:22:35 +0300

    change firstlook height

--- src/components/FirstLook/FirstLook.jsx
+++ src/components/FirstLook/FirstLook.jsx
@@ -9,7 +9,7 @@ const FirstLook = () => {
   const forMobile = window.innerWidth > 700;
 
   return (
-    <div className="md:max-w-6xl h-[35vh] md:h-[90vh] flex flex-col gap-16 md:justify-normal md:gap-[10rem]">
+    <div className="md:max-w-6xl h-[25vh] md:h-[90vh] flex flex-col gap-16 md:justify-normal md:gap-[10rem]">
       <div className="md:w-[50%] flex flex-col items-center justify-center md:items-start md:justify-normal gap-5 mt-10 md:mt-[10rem] ">
         <h1 className="text-xl md:text-4xl font-bold leading-snug">
           Modern Living For Everyone
@@ -20,7 +20,7 @@ const FirstLook = () => {
         </p>
       </div>
       <div className="flex flex-col gap-3">
-        <div className="w-[90%] mx-auto md:mx-0 md:w-[40rem] h-20 flex bg-gray-100 md:bg-gray-100/60 rounded-lg p-2 ">
+        <div className="w-[90%] mx-auto md:mx-0 md:w-[40rem] h-16 flex bg-gray-100 md:bg-gray-100/60 rounded-lg p-2 ">
           <input
             type="text"
             className="w-full h-full outline-none pl-5 bg-transparent md:text-lg"
